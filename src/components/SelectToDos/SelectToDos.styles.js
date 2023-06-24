970ea8b2e58b574b271df7e3fcd9004699f44763
Tarik Čahtarević <tarikcahtarevic@gmail.com>
2023-06-24 12:14:46 +0200

    change action bar styles

--- src/components/SelectToDos/SelectToDos.styles.js
+++ src/components/SelectToDos/SelectToDos.styles.js
@@ -1,23 +1,28 @@
 import styled from 'styled-components';
 
 export const StyledFilterButtons = styled.div`
-  margin-top: 0.25rem;
+  margin-top: 1rem;
   display: flex;
   height: 3.1rem;
   width: 100%;
   flex-direction: row;
   align-items: center;
   justify-content: space-around;
-  border-radius: 1rem;
-  background-color: hsl(0, 0%, 98%);
+  border-radius: 0.3rem;
+  color: ${(props) => props.theme.filterToDosColor};
+  background-color: ${(props) => props.theme.toDoBgColor};
   font-size: 0.875rem /* 14px */;
+  font-weight: 900;
   line-height: 1.25rem /* 20px */;
 `;
 
 export const StyledFilterButton = styled.button`
-  /* color: ;
-  background-color: ; */
   border: none;
   outline: none;
   cursor: pointer;
+
+  &:active,
+  &:focus {
+    color: hsl(220, 98%, 61%);
+  }
 `;
